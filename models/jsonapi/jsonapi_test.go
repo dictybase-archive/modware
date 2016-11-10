34c18ef06f8ca022249bbc376017f2eafb0869b5
Siddhartha Basu <biosidd@gmail.com>
2016-11-10 21:34:24 +0000

    Added test for default relationship links

--- models/jsonapi/jsonapi_test.go
+++ models/jsonapi/jsonapi_test.go
@@ -61,5 +61,35 @@ func TestSelfLink(t *testing.T) {
 }
 
 func TestRelationshipLink(t *testing.T) {
-
+	srvinfo := GetApiServerInfo()
+	bslink := generateBaseLink(srvinfo)
+	u := &User{
+		ID:    "32",
+		Name:  "Tucker",
+		Email: "tucker@jumbo.com",
+	}
+	pstruct, err := MarshalToStructWrapper(u, srvinfo)
+	if err != nil {
+		t.Errorf("error in marshaling to structure %s\n", err)
+	}
+	rel := jsapi.Relationship{
+		Links: &jsapi.Links{
+			Self:    fmt.Sprintf("%s/%s/%s/relationships/%s", bslink, "users", "32", "roles"),
+			Related: fmt.Sprintf("%s/%s/%s/%s", bslink, "users", "32", "roles"),
+		},
+	}
+	exstruct := &jsapi.Document{
+		Links: &jsapi.Links{Self: fmt.Sprintf("%s/%s/%s", bslink, "users", "32")},
+		Data: &jsapi.DataContainer{
+			DataObject: &jsapi.Data{
+				Type:          "users",
+				ID:            "32",
+				Attributes:    []byte(`{"name":"Tucker","email":"tucker@jumbo.com"}`),
+				Relationships: map[string]jsapi.Relationship{"roles": rel},
+			},
+		},
+	}
+	if !reflect.DeepEqual(pstruct, exstruct) {
+		t.Fatal("expected and generated jsonapi structure did not match")
+	}
 }
