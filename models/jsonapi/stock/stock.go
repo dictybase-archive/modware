7609e181ac036bd36e1775eee5ae2ae4d3a6d61f
Siddhartha Basu <biosidd@gmail.com>
2016-11-18 22:13:39 +0000

    Added phenotype relations Added genotype structure and relations

--- models/jsonapi/stock/stock.go
+++ models/jsonapi/stock/stock.go
@@ -1,6 +1,9 @@
 package stock
 
 import (
+	"fmt"
+
+	jsapi "github.com/dictybase/modware/models/jsonapi"
 	"github.com/manyminds/api2go/jsonapi"
 )
 
@@ -40,13 +43,15 @@ func (so *StockOrder) GetReferences() []jsonapi.Reference {
 }
 
 type Phenotype struct {
-	ID          string `json:"-"`
-	Name        string `json:"name"`
-	Observation string `json:"observation"`
-	Attribute   string `json:"phen_attribute"`
-	Value       string `json:"value"`
-	Cvalue      string `json:"cvalue"`
-	Evidence    string `json:"evidence"`
+	ID          string       `json:"-"`
+	Name        string       `json:"name"`
+	Observation string       `json:"observation"`
+	Attribute   string       `json:"phen_attribute"`
+	Value       string       `json:"value"`
+	Cvalue      string       `json:"cvalue"`
+	Evidence    string       `json:"evidence"`
+	Props       []*PhenoProp `json:"-"`
+	PropIDs     []string     `json:"-"`
 }
 
 func (ph *Phenotype) GetID() string {
@@ -56,4 +61,125 @@ func (ph *Phenotype) GetID() string {
 func (ph *Phenotype) SetID(id string) error {
 	ph.ID = id
 	return nil
+}
+
+func (ph *Phenotype) GetRelatedLinksInfo() []jsapi.RelationShipLink {
+	return []jsapi.RelationShipLink{
+		jsapi.RelationShipLink{Name: "properties"},
+	}
+}
+
+func (ph *Phenotype) GetReferencedStructs() (result []jsonapi.MarshalIdentifier) {
+	if len(ph.Props) > 0 {
+		for _, p := range ph.Props {
+			result = append(result, p)
+		}
+	}
+	return result
+}
+
+func (ph *Phenotype) SetToManyReferenceIDs(name string, IDs []string) error {
+	if name == "properties" {
+		ph.PropIDs = IDs
+		return nil
+	}
+	return fmt.Errorf("%s No such has many relationships", name)
+}
+
+type PhenoProp struct {
+	ID     string `json:"-"`
+	Value  string `json:"value"`
+	Cv     string `json:"-"`
+	TermID string `json:"-"`
+}
+
+func (prop *PhenoProp) GetID() string {
+	return prop.ID
+}
+
+func (prop *PhenoProp) SetID(id string) error {
+	prop.ID = id
+	return nil
+}
+
+func (prop *PhenoProp) GetName() string {
+	return "chadoprops"
+}
+
+func (prop *PhenoProp) GetRelatedLinksInfo() []jsapi.RelationShipLink {
+	return []jsapi.RelationShipLink{
+		jsapi.RelationShipLink{
+			Name:           "proptype",
+			SuffixFragment: fmt.Sprintf("cvs/%s/cvterms/%s", prop.Cv, prop.TermID),
+		},
+	}
+}
+
+type Genotype struct {
+	ID          string       `json:"-"`
+	Name        string       `json:"name"`
+	Description string       `json:"description"`
+	Props       []*PhenoProp `json:"-"`
+	PropIDs     []string     `json:"-"`
+}
+
+func (g *Genotype) GetID() string {
+	return g.ID
+}
+
+func (g *Genotype) SetID(id string) error {
+	g.ID = id
+	return nil
+}
+
+func (g *Genotype) GetRelatedLinksInfo() []jsapi.RelationShipLink {
+	return []jsapi.RelationShipLink{
+		jsapi.RelationShipLink{Name: "properties"},
+	}
+}
+
+func (g *Genotype) GetReferencedStructs() (result []jsonapi.MarshalIdentifier) {
+	if len(g.Props) > 0 {
+		for _, p := range g.Props {
+			result = append(result, p)
+		}
+	}
+	return result
+}
+
+func (g *Genotype) SetToManyReferenceIDs(name string, IDs []string) error {
+	if name == "properties" {
+		g.PropIDs = IDs
+		return nil
+	}
+	return fmt.Errorf("%s No such has many relationships", name)
+}
+
+type GenoProp struct {
+	ID     string `json:"-"`
+	Value  string `json:"value"`
+	Cv     string `json:"-"`
+	TermID string `json:"-"`
+}
+
+func (prop *GenoProp) GetID() string {
+	return prop.ID
+}
+
+func (prop *GenoProp) SetID(id string) error {
+	prop.ID = id
+	return nil
+}
+
+func (prop *GenoProp) GetName() string {
+	return "chadoprops"
+}
+
+func (prop *GenoProp) GetRelatedLinksInfo() []jsapi.RelationShipLink {
+	return []jsapi.RelationShipLink{
+		jsapi.RelationShipLink{
+			Name:           "proptype",
+			SuffixFragment: fmt.Sprintf("cvs/%s/cvterms/%s", prop.Cv, prop.TermID),
+		},
+	}
 }
