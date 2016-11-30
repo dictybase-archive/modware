50dd5a761d01dd803acb2fe72a72e19f3dbe3c2d
Siddhartha Basu <biosidd@gmail.com>
2016-11-30 03:44:37 +0000

    Added jsonapi error renderer

--- render/render.go
+++ render/render.go
@@ -3,12 +3,28 @@ package render
 import (
 	"encoding/json"
 	"net/http"
+	"strconv"
 
+	"github.com/manyminds/api2go"
 	"github.com/manyminds/api2go/jsonapi"
 )
 
-func renderJSONAPI(w http.ResponseWriter, status int, data *jsonapi.Document) error {
+func JSONAPI(w http.ResponseWriter, status int, data *jsonapi.Document) error {
 	w.Header().Set("Content-Type", "application/vnd.api+json")
 	w.WriteHeader(status)
 	return json.NewEncoder(w).Encode(data)
+}
+
+func JSONAPIError(w http.ResponseWriter, status int, err error, msg string) error {
+	w.Header().Set("Content-Type", "application/vnd.api+json")
+	w.WriteHeader(status)
+	jsnErr := api2go.Error{
+		Status: strconv.Itoa(status),
+		Title:  msg,
+		Detail: err.Error(),
+		Meta: map[string]interface{}{
+			"creator": "modware api",
+		},
+	}
+	return json.NewEncoder(w).Encode(api2go.HTTPError{Errors: []api2go.Error{jsnErr}})
 }
