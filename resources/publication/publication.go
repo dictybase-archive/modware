0dad603b74889c1562791b0fdc1b2c1aa9a9bd07
Siddhartha Basu <biosidd@gmail.com>
2016-12-01 10:39:53 +0000

    Fixed function argument to match the Resource interface

--- resources/publication/publication.go
+++ resources/publication/publication.go
@@ -13,23 +13,23 @@ func (pub *Publication) GetDbh() *sql.DB {
 	return pub.Dbh
 }
 
-func (pub *Publication) Get(w http.ResponseWriter, r *http.Response) {
+func (pub *Publication) Get(w http.ResponseWriter, r *http.Request) {
 
 }
 
-func (pub *Publication) GetAll(w http.ResponseWriter, r *http.Response) {
+func (pub *Publication) GetAll(w http.ResponseWriter, r *http.Request) {
 
 }
 
-func (pub *Publication) Create(w http.ResponseWriter, r *http.Response) {
+func (pub *Publication) Create(w http.ResponseWriter, r *http.Request) {
 
 }
 
-func (pub *Publication) Update(w http.ResponseWriter, r *http.Response) {
+func (pub *Publication) Update(w http.ResponseWriter, r *http.Request) {
 
 }
 
-func (pub *Publication) Delete(w http.ResponseWriter, r *http.Response) {
+func (pub *Publication) Delete(w http.ResponseWriter, r *http.Request) {
 
 }
 
@@ -41,22 +41,22 @@ func (a *Author) GetDbh() *sql.DB {
 	return a.Dbh
 }
 
-func (a *Author) Get(w http.ResponseWriter, r *http.Response) {
+func (a *Author) Get(w http.ResponseWriter, r *http.Request) {
 
 }
 
-func (a *Author) GetAll(w http.ResponseWriter, r *http.Response) {
+func (a *Author) GetAll(w http.ResponseWriter, r *http.Request) {
 
 }
 
-func (a *Author) Create(w http.ResponseWriter, r *http.Response) {
+func (a *Author) Create(w http.ResponseWriter, r *http.Request) {
 
 }
 
-func (a *Author) Update(w http.ResponseWriter, r *http.Response) {
+func (a *Author) Update(w http.ResponseWriter, r *http.Request) {
 
 }
 
-func (a *Author) Delete(w http.ResponseWriter, r *http.Response) {
+func (a *Author) Delete(w http.ResponseWriter, r *http.Request) {
 
 }
