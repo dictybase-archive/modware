e50af71a6740ca02511c8bf26571fdacd565fa83
Siddhartha Basu <biosidd@gmail.com>
2016-12-09 18:09:28 +0000

    Added a generic error handling method to manage errors cropping up after the database queries

--- render/render.go
+++ render/render.go
@@ -5,6 +5,9 @@ import (
 	"net/http"
 	"strconv"
 
+	"github.com/dictyBase/modware/resources"
+
+	"github.com/gocraft/dbr"
 	"github.com/manyminds/api2go"
 	"github.com/manyminds/api2go/jsonapi"
 )
@@ -27,4 +30,18 @@ func JSONAPIError(w http.ResponseWriter, status int, err error, msg string) erro
 		},
 	}
 	return json.NewEncoder(w).Encode(api2go.HTTPError{Errors: []api2go.Error{jsnErr}})
+}
+
+func GenericError(w http.ResponseWriter, err error) {
+	if err == dbr.ErrNotFound {
+		err := JSONAPIError(w, http.StatusNotFound, err, resources.ErrNotExist.Error())
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+		}
+	} else { // possible database query error
+		err := JSONAPIError(w, http.StatusInternalServerError, err, resources.ErrDatabaseQuery.Error())
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+		}
+	}
 }
