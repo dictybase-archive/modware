79cca34eb58dae4af1a42742a4a54f8011e15e9e
Siddhartha Basu <biosidd@gmail.com>
2016-12-09 21:06:52 +0000

    Added an implentation of jsonapi.ServerInformation type Added a resource interface method to retrieve jsonapi.ServerInformation

--- resources/resources.go
+++ resources/resources.go
@@ -5,6 +5,7 @@ import (
 	"net/http"
 
 	"github.com/gocraft/dbr"
+	"github.com/manyminds/api2go/jsonapi"
 )
 
 var (
@@ -12,11 +13,34 @@ var (
 	ErrNotExist      = errors.New("resource not found")
 )
 
+// Interface for every http resource to implement
 type Resource interface {
+	// Gets the database handler
 	GetDbh() *dbr.Connection
+	// Handles the http GET for singular resource
 	Get(http.ResponseWriter, *http.Request)
+	// Handles the http GET for collection resource
 	GetAll(http.ResponseWriter, *http.Request)
+	// Handles the http POST
 	Create(http.ResponseWriter, *http.Request)
+	// Handles the http PATCH
 	Update(http.ResponseWriter, *http.Request)
+	// Handles the http DELETE
 	Delete(http.ResponseWriter, *http.Request)
+	// Gets a jsonapi.ServerInformation implementing interface
+	GetApiServerInfo() jsonapi.ServerInformation
+}
+
+// Type that implements jsonapi.ServerInformation interface
+type ApiServer struct {
+	BaseUrl string
+	Prefix  string
+}
+
+func (server *ApiServer) GetBaseURL() string {
+	return server.BaseUrl
+}
+
+func (server *ApiServer) GetPrefix() string {
+	return server.Prefix
 }
