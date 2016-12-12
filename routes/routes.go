417ec887fe27077920168bd6f7f11e8af20c89a0
Siddhartha Basu <biosidd@gmail.com>
2016-12-12 18:56:54 +0000

    Added pagination middleware in the chain for http get on /publications path

--- routes/routes.go
+++ routes/routes.go
@@ -2,13 +2,14 @@ package routes
 
 import (
 	"github.com/dictyBase/go-middlewares/middlewares/chain"
+	"github.com/dictyBase/go-middlewares/middlewares/pagination"
 	"github.com/dictyBase/go-middlewares/middlewares/router"
 	"github.com/dictyBase/modware/resources"
 )
 
 func AddPublication(rs resources.Resource, mwChain chain.Chain, r *router.Wrapper) {
 	r.Get("/publications/:id", mwChain.ThenFunc(rs.Get))
-	r.Get("/publications", mwChain.ThenFunc(rs.GetAll))
+	r.Get("/publications", mwChain.Append(pagination.MiddlewareFn).ThenFunc(rs.GetAll))
 	r.Post("/publications", mwChain.ThenFunc(rs.Create))
 	r.Patch("/publications/:id", mwChain.ThenFunc(rs.Update))
 	r.Delete("/publication/:id", mwChain.ThenFunc(rs.Delete))
