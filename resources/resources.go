43fd723ec3f03739e7d5234a96c969f57dc202f2
Siddhartha Basu <biosidd@gmail.com>
2016-12-16 20:24:44 +0000

    Added scheme to base url

--- resources/resources.go
+++ resources/resources.go
@@ -2,6 +2,7 @@ package resources
 
 import (
 	"errors"
+	"fmt"
 	"net/http"
 
 	"github.com/dictyBase/go-middlewares/middlewares/pagination"
@@ -55,7 +56,7 @@ func (server *APIServer) GetPrefix() string {
 // GetAPIServerInfo returns an implementation of jsonapi.ServerInformation
 func GetAPIServerInfo(r *http.Request, prefix string) jsonapi.ServerInformation {
 	return &APIServer{
-		BaseURL: r.URL.Host,
+		BaseURL: fmt.Sprintf("%s/%s", r.URL.Scheme, r.URL.Host),
 		Prefix:  prefix,
 	}
 }
