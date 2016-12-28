04e9a802d032d2df03a424ac6205376abc620704
Siddhartha Basu <biosidd@gmail.com>
2016-12-28 13:43:19 +0000

    Refactored the ExpectBuilder,RequestBuilder and ResponseBuilder implementations

---
+++ modwaretest/expect.go
@@ -0,0 +1,35 @@
+package modwaretest
+
+import (
+	"fmt"
+	"net/http/httptest"
+
+	"github.com/dictyBase/modware/resources"
+)
+
+// ExpectBuilder interface is for incremental building of http configuration
+type ExpectBuilder interface {
+	Get(string) RequestBuilder
+}
+
+// HTTPExpectBuilder implements ExpectBuilder interface
+type HTTPExpectBuilder struct {
+	reporter Reporter
+	host     string
+	resource resources.Resource
+}
+
+// NewHTTPExpectBuilder is the constructor for HTTPExpectBuilder
+func NewHTTPExpectBuilder(rep Reporter, host string, rs resources.Resource) ExpectBuilder {
+	return &HTTPExpectBuilder{
+		reporter: rep,
+		host:     host,
+		resource: rs,
+	}
+}
+
+// Get configures Request to execute a http GET request
+func (b *HTTPExpectBuilder) Get(path string) RequestBuilder {
+	req := httptest.NewRequest("GET", fmt.Sprintf("%s/%s", b.host, path), nil)
+	return NewHTTPRequestBuilder(b.reporter, req, b.resource.Get)
+}
