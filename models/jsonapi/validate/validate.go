d1735d90f2e5a5f20e2201431695cc9d6910f6ea
Siddhartha Basu <biosidd@gmail.com>
2017-01-13 20:31:38 +0000

    generate error when no relationship is present

--- models/jsonapi/validate/validate.go
+++ models/jsonapi/validate/validate.go
@@ -10,21 +10,27 @@ import (
 //Relationships matches if the relationships are implemented in the
 //given JSONAPI implementing type
 func Relationships(data interface{}, rels []string) error {
+	hasSelf, hasRelated := false
 	self, ok := data.(jsonapi.MarshalSelfRelations)
 	if ok {
 		for _, rel := range self.GetSelfLinksInfo() {
 			if !aphcollection.Contains(rels, rel.Name) {
-				return fmt.Errorf("cannot match %s self relationship", rel.Name)
+				return fmt.Errorf("cannot match %s to self relationship", rel.Name)
 			}
 		}
+		hasSelf = true
 	}
 	related, ok := data.(jsonapi.MarshalRelatedRelations)
 	if ok {
 		for _, rel := range related.GetRelatedLinksInfo() {
 			if !aphcollection.Contains(rels, rel.Name) {
-				return fmt.Errorf("cannot match %s self relationship", rel.Name)
+				return fmt.Errorf("cannot match %s to self relationship", rel.Name)
 			}
 		}
+		hasRelated = true
+	}
+	if !hasSelf && !hasRelated {
+		return fmt.Errorf("no self or related relationship defined")
 	}
 	return nil
 }
