ac7c8dcd6a5a1df46bc24ad46b6eea699ab04ddb
Siddhartha Basu <biosidd@gmail.com>
2017-01-13 19:18:46 +0000

    Added a validation package for jsonapi models

---
+++ models/jsonapi/validate/validate.go
@@ -0,0 +1,30 @@
+package validate
+
+import (
+	"fmt"
+
+	"github.com/dictyBase/apihelpers/aphcollection"
+	"github.com/dictyBase/modware/models/jsonapi"
+)
+
+//ValidateRelationships matches if the relationships are implemented in the
+//given JSONAPI implementing type
+func ValidateRelationships(data interface{}, rels []string) error {
+	self, ok := data.(jsonapi.MarshalSelfRelations)
+	if ok {
+		for _, rel := range self.GetSelfLinksInfo() {
+			if !aphcollection.Contains(rels, rel.Name) {
+				return fmt.Errorf("cannot match %s self relationship", rel.Name)
+			}
+		}
+	}
+	related, ok := data.(jsonapi.MarshalRelatedRelations)
+	if ok {
+		for _, rel := range related.GetRelatedLinksInfo() {
+			if !aphcollection.Contains(rels, rel.Name) {
+				return fmt.Errorf("cannot match %s self relationship", rel.Name)
+			}
+		}
+	}
+	return nil
+}
