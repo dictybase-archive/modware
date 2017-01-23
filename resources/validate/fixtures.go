03dac1659889dd39111179f8ef0d8654f25f7305
Siddhartha Basu <biosidd@gmail.com>
2017-01-23 19:35:29 +0000

    added jsonapi implementing test structures

---
+++ resources/validate/fixtures.go
@@ -0,0 +1,83 @@
+package validate
+
+import "fmt"
+import jsapi "github.com/dictyBase/apihelpers/aphjsonapi"
+
+type Permission struct {
+	ID          string `json:"-"`
+	Permission  string `json:"permission"`
+	Description string `json:"description"`
+}
+
+func (p Permission) GetID() string {
+	return p.ID
+}
+
+type Role struct {
+	ID          string        `json:"-"`
+	Role        string        `json:"role"`
+	Description string        `json:"description"`
+	Permissions []*Permission `json:"-"`
+	Users       []*User       `json:"-"`
+}
+
+func (r *Role) GetID() string {
+	return r.ID
+}
+
+func (r *Role) GetSelfLinksInfo() []jsapi.RelationShipLink {
+	return []jsapi.RelationShipLink{
+		jsapi.RelationShipLink{Name: "users", Type: "users"},
+		jsapi.RelationShipLink{Name: "permissions", Type: "users"},
+	}
+}
+
+func (r *Role) ValidateSelfLinks() error {
+	return nil
+}
+
+func (r *Role) GetRelatedLinksInfo() []jsapi.RelationShipLink {
+	return []jsapi.RelationShipLink{
+		jsapi.RelationShipLink{
+			Name:           "users",
+			SuffixFragment: fmt.Sprintf("%s/%s/%s", "roles", r.GetID(), "consumers"),
+			Type:           "users",
+		},
+		jsapi.RelationShipLink{Name: "permissions", Type: "permissions"},
+	}
+}
+
+func (r *Role) ValidateRelatedLinks() error {
+	return nil
+}
+
+type User struct {
+	ID    string  `json:"-"`
+	Name  string  `json:"name"`
+	Email string  `json:"email"`
+	Roles []*Role `json:"-"`
+}
+
+func (u *User) GetID() string {
+	return u.ID
+}
+
+func (u *User) GetSelfLinksInfo() []jsapi.RelationShipLink {
+	return []jsapi.RelationShipLink{
+		jsapi.RelationShipLink{Name: "roles", Type: "roles"},
+	}
+}
+
+func (u *User) ValidateSelfLinks() error {
+	return nil
+}
+
+func (u *User) GetRelatedLinksInfo() []jsapi.RelationShipLink {
+	return []jsapi.RelationShipLink{
+		jsapi.RelationShipLink{Name: "roles", Type: "roles"},
+	}
+}
+
+func (u *User) ValidateRelatedLinks() error {
+	return nil
+}
